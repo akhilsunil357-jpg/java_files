0c1ab24cf9d1216fefa8b6284ae2ffab2ca4e359
Akhil sunil <akhilsunil357@gmail.com>
2026-03-24 14:17:19 +0530

    ShopBill

--- ShopBill.java
+++ ShopBill.java
@@ -1,10 +1,15 @@
-
-
 public class ShopBill {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("helloworld");
+		String item ="Soap";
+		int Quantity = 4;
+		Double Unit_price = 18.75;
+		Double Total_price = Quantity * Unit_price;
+		
+		System.out.println("Item name = Soap");
+		
+		System.out.printf(" Total price = %f",Total_price);
 	}
 		
 	
